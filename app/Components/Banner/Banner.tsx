64459f662dfbe9b30b03ca3579137d88c7c97244
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-07-19 11:37:11 +0530

    btn added

--- app/Components/Banner/Banner.tsx
+++ app/Components/Banner/Banner.tsx
@@ -31,6 +31,15 @@ const Banner = () => {
       <div className="min-h-screen w-full flex flex-col justify-between items-center px-4 py-8 md:py-12">
         <div></div>
         <div className="flex flex-col gap-6 items-center justify-center w-full">
+          <div
+            style={{ backgroundColor: "#452E2E" }}
+            className="p-2 text-white gap-2 font-medium rounded-full flex justify-center items-center"
+          >
+            <div className="px-2 py-0.5 bg-orange-600 rounded-full">
+              Educators
+            </div>
+            <div className="px-2 py-0.5  rounded-full">Schools</div>
+          </div>
           <div className="flex flex-col md:flex-row gap-6 md:gap-8 items-center">
             <img
               src="/Home/heroText.png"
@@ -81,7 +90,10 @@ const Banner = () => {
               />
             </div>
           </div>
-          <div className="text-lg md:text-xl font-semibold text-white/90 text-center">
+          <div
+            style={{ backgroundColor: "#452E2E" }}
+            className="px-4 py-1 rounded-full text-lg md:text-xl font-semibold text-white/90 text-center"
+          >
             PARTNER WITH US
           </div>
           <div>
